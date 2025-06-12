ad8925c92f29f3fcc53ffaf69776e0d817ccfddf
Takaya-Ishii <takaya-ishii@x-power.co.jp>
2025-06-12 14:10:53 +0900

    LoginFormを作成

---
+++ src/main/java/com/example/demo/form/LoginForm.java
@@ -0,0 +1,12 @@
+package com.example.demo.form;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+   /** ユーザーID */
+   private String usernameInput;
+   /** パスワード */
+   private String passwordInput;
+}
+  
