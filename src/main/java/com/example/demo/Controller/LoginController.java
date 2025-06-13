76d977d0be4ad3882b96184c19d03a4f6898ccd8
Takaya-Ishii <takaya-ishii@x-power.co.jp>
2025-06-13 18:24:11 +0900

    ログイン画面からユーザー一覧または受講研修一覧画面へ遷移するようになりました。
    MTGで言ってた関数名の問題も解決しました

--- src/main/java/com/example/demo/Controller/LoginController.java
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.demo.Controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,11 +9,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.demo.form.LoginForm;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("")
 public class LoginController {
 	
-	 @GetMapping
-	    public String showLogin(@ModelAttribute LoginForm form) {
+	 @GetMapping("/login")
+	    public String displayLogin(@ModelAttribute LoginForm form) {
 	        return "login";
+	 }
+	 
+	 @GetMapping("/default")
+	    public String defaultAfterLogin(Authentication authentication) {
+	        if (authentication.getAuthorities().stream()
+	                .anyMatch(auth -> auth.getAuthority().equals("ROLE_講師"))) {
+	            return "redirect:/admin/User";
+	        } else if (authentication.getAuthorities().stream()
+	                .anyMatch(auth -> auth.getAuthority().equals("ROLE_受講者"))) {
+	            return "redirect:/participant/traCourse";
+	        }
+	        return "redirect:/";
+	    }
+	 
+	 @GetMapping("/admin/User")
+	 public String displayAllUser() {
+		 return "/admin/User";
+	 }
+	 
+	 @GetMapping("/participant/traCourse")
+	 public String displaytraCourse() {
+		 return "/participant/traCourse";
 	 }
 }
