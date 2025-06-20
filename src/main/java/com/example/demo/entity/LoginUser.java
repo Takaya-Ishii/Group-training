9dcec24c1c9c7891fd907907c0e98643db91327d
Takaya-Ishii <takaya-ishii@x-power.co.jp>
2025-06-20 17:51:21 +0900

    Authentication.javaに追加した分を追加

--- src/main/java/com/example/demo/entity/LoginUser.java
+++ src/main/java/com/example/demo/entity/LoginUser.java
@@ -5,14 +5,71 @@ import java.util.Collection;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 /**
-* ユーザーの認証情報を表すUserDetails実装クラス
-*/
+* ユーザーの認証情報を表すUser実装クラス*/
+
 public class LoginUser extends User {
-   /** 最低限の情報を保持したUserDetails
+   private String account_name;
+   private String address;
+   private String gender;
+   private String TEL;
+   private String affiriation;
+   private String departOfOrigin;
+   private int role_ID;
+   private int group_ID;
+
+/** ユーザーの情報を保持したUser
     *  実装クラスUserを作成する */
    public LoginUser(String username,
      String password,
-     Collection<? extends GrantedAuthority> authorities) {
+     Collection<? extends GrantedAuthority> authorities,
+     String account_name,
+     String address,
+     String gender,
+     String TEL,
+     String affiriation,
+     String departOfOrigin,
+     int role_ID,
+     int group_ID) {
        super(username, password, authorities);
+       this.account_name = account_name;
+       this.address = address;
+       this.gender = gender;
+       this.TEL = TEL;
+       this.affiriation = affiriation;
+       this.departOfOrigin = departOfOrigin;
+       this.role_ID = role_ID;
+       this.group_ID = group_ID;
    }
+	
+    public String getAccount_name() {
+        return this.account_name;
+    }
+
+    public String getTEL() {
+        return this.TEL;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+    
+    public String getAffiriation() {
+    	return this.affiriation;
+    }
+    
+    public String getDepartOfOrigin() {
+    	return this.departOfOrigin;
+    }
+    
+    public int getRole_ID() {
+    	return this.role_ID;
+    }
+    
+    public int getGroup_ID() {
+    	return this.group_ID;
+    }
 }
