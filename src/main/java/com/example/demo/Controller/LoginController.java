5b2cb96a01119a4e1ed06b78026fd6f6f9392a18
KatoKotaro-system-global:-local <kotaro-kato@x-power.co.jp>
2025-06-25 15:25:28 +0900

    20250625マージコミットテスト

--- src/main/java/com/example/demo/Controller/LoginController.java
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -1,18 +1,47 @@
 package com.example.demo.Controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.entity.LoginUser;
 import com.example.demo.form.LoginForm;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("")
 public class LoginController {
 	
-	 @GetMapping
-	    public String showLogin(@ModelAttribute LoginForm form) {
+	// ログイン画面の表示
+	 @GetMapping("/login")
+	    public String displayLogin(@ModelAttribute LoginForm form) {
 	        return "login";
+	 }
+	 
+	 // 所持ロールによって遷移先を判別
+	 @GetMapping("/default")
+	    public String defaultAfterLogin(Model model,  @AuthenticationPrincipal LoginUser loginUser) {
+		 model.addAttribute("LoginUser", loginUser);   
+		 if (loginUser.getAuthorities().stream()
+	                .anyMatch(auth -> auth.getAuthority().equals("ROLE_講師"))) {
+	            return "redirect:admin/User";
+	        } else if (loginUser.getAuthorities().stream()
+	                .anyMatch(auth -> auth.getAuthority().equals("ROLE_受講者"))) {
+	            return "redirect:participant/traCourse";
+	        }
+	        return "redirect:/";
+	    }
+	 
+	 // 以下テスト用(受講者が講師専用画面に遷移できるか、講師が受講者の画面も表示できるか)
+	 @GetMapping("/admin/User")
+	 public String displayAllUser() {
+		 return "admin/User";
+	 }
+	 
+	 @GetMapping("/participant/traCourse")
+	 public String displaytraCourse() {
+		 return "participant/traCourse";
 	 }
 }
--- src/main/java/com/example/demo/entity/Authentication.java
+++ src/main/java/com/example/demo/entity/Authentication.java
@@ -10,6 +10,20 @@ import lombok.NoArgsConstructor;
 public class Authentication {
    /** ユーザーID */
    private String username;
+   /** ユーザー名 */
+   private String account_name;
    /** パスワード */
    private String password;
+   /** 電話番号 */
+   private String TEL;
+   /** 住所 */
+   private String address;
+   /** 性別 */
+   private String gender;
+   /** 所属 */
+   private String affiriation;
+   /** 出身学部 */
+   private String departOfOrigin;
+   /** ロールID */
+   private int role_ID;
 }
