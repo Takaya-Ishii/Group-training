2baa7805999e99d701827d734f07c3be34709230
Yumika <yumika.kirishiki@x-power.co.jp>
2025-06-26 11:58:44 +0900

    研修進捗管理機能に関するファイルを追加

---
+++ src/main/java/com/example/demo/form/StackForm.java
@@ -0,0 +1,50 @@
+package com.example.demo.form;
+
+import java.util.Date;
+
+import jakarta.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.example.demo.validator.StudyTimeCheck;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 学習履歴：Form
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StackForm {
+	//履歴No
+	private Integer stackNo;
+
+	//受講研修ID
+	private Integer traCourseID;
+	
+	//研修名
+	private String traName;
+	
+	//学習した日付
+	@NotNull (message = "学習した日付を入力してください")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date studyDay;
+	
+	//学習時間
+	@StudyTimeCheck
+	private Double studyTime;
+	
+	//進捗
+	@NotNull (message = "進捗率を入力してください")
+	private Integer progress;
+	
+	//メモ
+	private String memo;
+	
+	//ステータスID
+	private Integer statusID;
+}
