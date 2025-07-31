889cfd105661a43c23478ead0a60fc3bdfe2dd68
Takaya-Ishii <takaya-ishii@x-power.co.jp>
2025-07-31 14:54:02 +0900

    model.addattributeでグループを渡したい

--- src/main/java/com/example/demo/Controller/LoginController.java
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,6 +32,7 @@ public class LoginController {
 	            return "redirect:admin/User";
 	        } else if (loginUser.getAuthorities().stream()
 	                .anyMatch(auth -> auth.getAuthority().equals("ROLE_受講者"))) {
+	        	model.addAttribute("group", authenticationMapper.selectGroupByUsername(loginUser.getUsername()));
 	            return "redirect:participant/traCourse/"+loginUser.getUsername();
 	        }
 	        return "redirect:/";
