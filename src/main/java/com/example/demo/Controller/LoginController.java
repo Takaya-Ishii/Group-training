e0dce437df4a36e8ae938fd6c8720886a83c63dd
Hibiki-Hamada <hibiki-hamada@x-power.co.jp>
2025-07-31 14:43:07 +0900

    調整その２

--- src/main/java/com/example/demo/Controller/LoginController.java
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.demo.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,10 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.entity.LoginUser;
 import com.example.demo.form.LoginForm;
+import com.example.demo.repository.AuthenticationMapper;
 
 @Controller
 @RequestMapping("")
 public class LoginController {
+	
+	@Autowired
+	AuthenticationMapper authenticationMapper;
 	
 	// ログイン画面の表示
 	 @GetMapping("/login")
